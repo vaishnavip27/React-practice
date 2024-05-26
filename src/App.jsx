362d86c59e3fb88df77c6fe8c7533fd0992e62c9
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-05-27 00:10:05 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
@@ -8,7 +10,7 @@ function Header() {
   const description = reactDescriptions[getRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -18,12 +20,46 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt="props.title" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block"
+              image={componentsImg}
+            />
+            <CoreConcept
+              title="Props"
+              description="Using components with different data"
+              image={componentsImg}
+            />
+            <CoreConcept
+              title="styling"
+              description="Styling all the static website"
+              image={componentsImg}
+            />
+            <CoreConcept
+              title="coding"
+              description="Making the website interactive"
+              image={componentsImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
