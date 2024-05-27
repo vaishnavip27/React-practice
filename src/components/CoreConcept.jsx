3107faa48decd8218192620c934a40489e68fd17
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-05-27 23:55:00 +0530

    changes

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,9 +1,9 @@
-export default function CoreConcept(props) {
+export default function CoreConcept({ image, title, description }) {
   return (
     <li>
-      <img src={props.image} alt="props.title" />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt="props.title" />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
